2a8f2bd61b5a8e27d17c77b187cf9d6b8350f998
nidaa-awawdeh <nidaaawawdeh@outlook.sa>
2020-04-01 01:34:08 +0300

    Create All Components. #2

---
+++ src/Components/Weather.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Weather extends Component {
+  render() {
+    return (
+      <div>
+        Weather Component
+        {/*  this component  is where the data will be displayed From API*/}
+      </div>
+    );
+  }
+}
+
+export default Weather;
