43fc4ef2bfc49bdd46d2e8aa90faa8d718648c37
aboodswiti <swaiti393@gmail.com>
2020-04-02 02:39:34 +0300

    create gify Api

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Weather.css";
-import WeatherCondition from './WeatherCondition';
+
+import WeatherCondition from "./WeatherCondition";
 
 let url = "";
 class Weather extends Component {
@@ -9,7 +10,9 @@ class Weather extends Component {
     error: null,
     lat: null,
     log: null,
-
+    gif: "",
+    gif1: "",
+    gif2: "",
     text: "",
     data: {
       wind: "",
@@ -29,23 +32,50 @@ class Weather extends Component {
         let latitude = position.coords.latitude;
         let longitude = position.coords.longitude;
         this.setState({ lat: latitude, log: longitude });
-        console.log(this.state.lat, "position");
         url = `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lat}139&units=metric&apikey=cf9fb4df751879d3f30929a3dd9050e4`;
         this.handleFetch(url);
-        console.log(url, "position");
       });
 
-      // this.handleSubmit(e)
     }
   }
 
   handleSubmit = (e) => {
-  
-       url = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.text}&units=metric&apikey=cf9fb4df751879d3f30929a3dd9050e4`;
+    if (!this.state.text.length) {
+      return;
+    }
+    this.setState({ error: null });
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.text}&units=metric&apikey=cf9fb4df751879d3f30929a3dd9050e4`;
 
     e.preventDefault();
     this.handleFetch(url);
+  };
 
+  getGify = (city) => {
+    if (!city  || city ==='Fāraskūr') {
+      return;
+    }
+
+    let Gif_API = `http://api.giphy.com/v1/gifs/search?q=${city}&limit=3&api_key=PjePAILYBVdogMvZdg6PaRPNAQoLmbIX`;
+
+    this.setState({ isLoading: true });
+
+    fetch(Gif_API)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Something went wrong 2 ...");
+        }
+      })
+      .then((result) => {
+        this.setState({
+          isLoading: false,
+          gif: result.data[0].images.fixed_height.url,
+          gif1: result.data[1].images.fixed_height.url,
+          gif2: result.data[2].images.fixed_height.url
+        });
+      })
+      .catch((error) => this.setState({ error, isLoading: false }));
   };
 
   handleFetch = (api1) => {
@@ -60,7 +90,8 @@ class Weather extends Component {
         }
       })
       .then((data) => {
-        this.setState({ isLoading: false, data: data });
+        this.setState({ isLoading: false, data: data, cityy: data.name });
+        this.getGify(data.name);
       })
       .catch((error) => this.setState({ error, isLoading: false }));
   };
@@ -68,30 +99,23 @@ class Weather extends Component {
   //...
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
+      <div className="InputDiv">
+        <form>
           <label>
-            Name:
             <input
+            className="InputField"
               type="text"
               value={this.state.value}
               onChange={this.handleChange}
-            />
+            placeholder=" Enter City / Country:"/>
           </label>
-          <input type="submit" value="Submit" />
+          <input className="Add" type="submit" onClick={this.handleSubmit} value="Submit"  />
         </form>
-        <WeatherCondition
-          items={this.state.data}
-          fetch={this.state}
-          // wind={this.state.wind}
-
-          // removeItem={this.removeItem}
-          // handleChange={this.handleChange}
-        />
+        <WeatherCondition items={this.state.data} fetch={this.state} />
+        <div></div>
       </div>
     );
   }
 }
-
 
 export default Weather;
